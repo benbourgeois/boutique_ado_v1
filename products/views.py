c550bb6d2189df14d2ff9fcc72e371efe542774c
benbourgeois <bourgeoisben@ymail.com>
2020-07-14 05:23:44 +0000

    Added Product Detail Page

--- products/views.py
+++ products/views.py
@@ -14,10 +14,6 @@ def all_products(request):
     return render(request, 'products/products.html', context)
 
 
-from django.shortcuts import render
-from .models import Product
-
-
 def product_detail(request, product_id):
     """ A view to show individual product details """
 
